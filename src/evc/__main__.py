6e6449635f0eb457f273c184d062eda1c4b70440
Filipe Verri <filipeverri@gmail.com>
2024-06-13 11:14:02 -0300

    Dynamically created Makefile.

--- src/evc/__main__.py
+++ src/evc/__main__.py
@@ -5,10 +5,42 @@ import numpy
 import json
 import pandas as pd
 from git import Repo
+import tempfile
+
+metamk_contents = """
+DATA_FILES = $(shell find src/data -type f)
+SAMPLING_FILES = $(shell find src/sampling -type f)
+EVALUATION_FILES = $(shell find src/evaluation -type f)
+MODEL_FILES = $(shell find src/model -type f)
+
+all: .meta/data.json .meta/sampling.json .meta/evaluation.json .meta/model.json
+
+.meta/:
+	@mkdir -p .meta
+
+.meta/data.json: .meta/ $(DATA_FILES)
+	@echo "Generating $@..."
+	@python -m evc meta --target data
+
+.meta/sampling.json: .meta/data.json $(SAMPLING_FILES)
+	@echo "Generating $@..."
+	@python -m evc meta --target sampling
+
+.meta/evaluation.json: .meta/ $(EVALUATION_FILES)
+	@echo "Generating $@..."
+	@python -m evc meta --target evaluation
+
+.meta/model.json: .meta/ $(MODEL_FILES)
+	@echo "Generating $@..."
+	@python -m evc meta --target model
+"""
 
 def update(args):
-    print('Updating metadata...')
-    os.system('make -f utils/meta.mk')
+    with tempfile.NamedTemporaryFile(suffix='.mk') as f:
+        f.write(metamk_contents.encode())
+        f.flush()
+        print('Updating metadata...')
+        os.system(f'make -f {f.name}')
 
 def meta(args):
     if args.target == 'data':
